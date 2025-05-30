e18e88370404e2184ac4b4d70db2202a6141d3fe
giuseppeesposito22 <gius.espo22@gmail.com>
2025-05-30 13:08:11 +0200

    add pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+
+import AboutPage from "./pages/AboutPage";
+
+import PostsPage from "./pages/PostsPage";
+
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/posts" element={<PostsPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
