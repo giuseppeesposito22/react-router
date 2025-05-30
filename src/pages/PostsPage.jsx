fe8ee97ef415ad0b887b5cbfc7b60ef20926aae4
giuseppeesposito22 <gius.espo22@gmail.com>
2025-05-30 19:32:48 +0200

    add player card component in posts page and generate card list

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,3 +1,17 @@
+import napoliTeam from "../data/napoliTeam";
+import PlayerCard from "../components/PlayerCard";
+
 export default function PostsPage() {
-  return <h1>Questa è la pagina dei posts</h1>;
+  return (
+    <>
+      <h1 className="text-center my-2">
+        La rosa del Napoli campione in Italia
+      </h1>
+      <div className="row row-cols-4 my-5 g-5">
+        {napoliTeam.map((player) => (
+          <PlayerCard key={player.id} player={player} />
+        ))}
+      </div>
+    </>
+  );
 }
