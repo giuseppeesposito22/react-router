af2c391756341626ef60dacbb2923217d08b6caa
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-03 17:29:39 +0200

    add link in PostCard

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function PostCard({ post }) {
   return (
     <div className="col">
@@ -8,11 +10,11 @@ export default function PostCard({ post }) {
           alt={post.title}
         />
         <div className="card-body">
-          <p className="card-text d-flex flex-column align-items-center justify-content-between h-100 gap-3">
+          <p className="card-text d-flex flex-column align-items-center justify-content-between">
             <span className="text-center fw-bold fs-4">{post.title}</span>
-
-            <span>{post.content}</span>
           </p>
+
+          <Link to={`/posts/${post.id}`}>vai al dettaglio</Link>
         </div>
       </div>
     </div>
